f3654831db065f603d739b4926ae5e15cc597300
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-16 14:03:23 -0500

    Update Chapter5View.swift

--- SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
+++ SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
@@ -1,11 +1,30 @@
-import SwiftUI
-
-struct Chapter5View: View {
+struct Chapter2View: View {
+    @State var showingChapters = false
     var body: some View {
-        Text("Chapter 5")
-    }
-}
+                VStack {
+                    HStack {
+                        Button(action: { showingChapters = true }) {
+                            Image(systemName: "book.fill")
+                                .font(.largeTitle)
+                        }
+                        .padding(.horizontal, 100)
+                        .sheet(isPresented: $showingChapters) {
+                            ChaptersView()
+                        }
+                        //....The Rest of our code 
 
-#Preview {
-    Chapter5View()
+
+struct ChaptersView: View {
+    var body: some View {
+        NavigationView {
+                    List {
+                        NavigationLink("Cover", destination: CoverView())
+                        NavigationLink("Chapter 1", destination: Chapter1View())
+                        NavigationLink("Chapter 2", destination: Chapter2View())
+                        NavigationLink("Chapter 3", destination: Chapter3View())
+                        NavigationLink("Chapter 4", destination: Chapter4View())
+                    }
+                    .navigationBarTitle("Chapters")
+                }
+    }
 }
