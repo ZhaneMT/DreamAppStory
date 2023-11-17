40b5eb56b4799616fa73c3dfe08643dbf38a0c63
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-17 13:13:42 -0500

    Update Chapter2View.swift

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -9,8 +9,8 @@ struct Chapter2View: View {
     @State private var showBackstory = false
     @State private var showSeafoods = false
     @State private var selectedFavoriteSeafood: favoriteSeafood = .lobsterTail
-    @State private var displayText: Text = Text("Select a food option to see details.")
-
+    @State private var displayText: Text = Text("What food should she eat")
+    
     // Enum for the vegan foods she likes to eat
     enum favoriteSeafood {
         case lobsterTail, shrimp, crablegs, salmon
@@ -35,75 +35,106 @@ struct Chapter2View: View {
     }
     
     var body: some View {
-                VStack {
-                    HStack {
-                        Button(action: { showingChapters = true }) {
-                            Image(systemName: "book.fill")
-                                .font(.largeTitle)
-                        }
-                        .padding(.horizontal, 100)
-                        .sheet(isPresented: $showingChapters) {
-                            ChaptersView()
-                        }
-                        Image(systemName: "door.right.hand.open")
+        NavigationView {
+            VStack {
+                HStack {
+                    Button(action: { showingChapters = true }) {
+                        Image(systemName: "book.fill")
                             .font(.largeTitle)
-                            .frame(width: 260 , height:0)
                     }
-                    .padding(.bottom, -58)
-
-            Text("Chapter 2: Title")
-                .font(.largeTitle)
-                .bold()
-                .underline()
-                .multilineTextAlignment(.leading)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                .padding(.top, 15)
-            Divider()
-            ScrollView{
-                
-                if showBackstory {
-                    Text("Insert 1 random text that foes into here. The best part about food is that it is delicious.")
-                        .padding()
-                }
-
-                Button(showBackstory ? "Close the backstory" : "Click here to read the backstory") {
-                    showBackstory.toggle()
-                }
-                
-                Text("Insert 1 random text that foes into here. The best part about food is that it is delicious.")
-                    .padding()
-                Text("Insert random text that foes into here. The best part about food is that it is delicious.")
-                    .padding()
-                Image("lol")
-                    .padding(.bottom, 20)
-                
-                
-                if showSeafoods {
-                    
-                    displayText
-                        .padding()
-                    
-                    Picker("Choose your food", selection: $selectedFavoriteSeafood) {
-                        Text("Lobster Tail").tag(favoriteSeafood.lobsterTail)
-                        Text("Crablegs").tag(favoriteSeafood.crablegs)
-                        Text("Shrimp").tag(favoriteSeafood.shrimp)
-                        Text("Salmon").tag(favoriteSeafood.salmon)
-
+                    .padding(.horizontal, 100)
+                    .sheet(isPresented: $showingChapters) {
+                        ChaptersView()
                     }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .onChange(of: selectedFavoriteSeafood) { newValue in
-                        seafoodText()
-                    }
+                    Image(systemName: "door.right.hand.open")
+                        .font(.largeTitle)
+                        .frame(width: 260 , height:0)
                 }
+                .padding(.bottom, -58)
                 
-                Button(showSeafoods ? "Close" : "Click here to choose what seafood she should eat Lobster Tail, Crablegs, Shrimp or Salmon?") {
-                    showSeafoods.toggle()
-                }
+                Text("Chapter 2: Being Tasha Jane")
+                    .font(.title)
+                    .bold()
+                    .underline()
+                    .multilineTextAlignment(.leading)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                    .padding(.top, 15)
                 Divider()
+                ScrollView{
+                    
+                    if showBackstory {
+                        Text("Everyone have experienced childhood things that they may have changed due to an adult or probably even still hold those habits with them.")
+                            .padding()
+                    }
+                    
+                    Button(showBackstory ? "Close the backstory" : "Click here to read the backstory") {
+                        showBackstory.toggle()
+                    }
+                    .padding()
+                    
+                    Text("\(mainCharacter) was a seafood lover and enjoyed working out for her apperance.")
+                        .padding()
+
+                    Text("Eventually she gained balance in both areas eating,working out and fasting, after a bad break up cause self-recoginiton.")
+                        .padding()
+                    Image("SZA")
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                        .padding(.bottom, 20)
+                    
+                    Text("\(mainCharacter) was hungry and wanted some food.")
+                        .padding()
+                    Image("seafood")
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                        .padding(.bottom, 20)
+                    
+                    
+                    if showSeafoods {
+                        
+                        displayText
+                            .padding()
+                        
+                        Picker("Choose your food", selection: $selectedFavoriteSeafood) {
+                            Text("Lobster Tail").tag(favoriteSeafood.lobsterTail)
+                            Text("Crablegs").tag(favoriteSeafood.crablegs)
+                            Text("Shrimp").tag(favoriteSeafood.shrimp)
+                            Text("Salmon").tag(favoriteSeafood.salmon)
+                            
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .onChange(of: selectedFavoriteSeafood) { newValue in
+                            seafoodText()
+                        }
+                    }
+                    
+                    Button(showSeafoods ? "Close" : "Click here to choose what seafood she should eat Lobster Tail, Crablegs, Shrimp or Salmon?") {
+                        showSeafoods.toggle()
+                    }
+                    Divider()
+                    HStack{
+                        NavigationLink(destination: Chapter1View()) {
+                            Label("", systemImage:"arrowshape.left.circle.fill")
+                        }
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                        Text("Oh Okay...")
+                            .foregroundColor(.white)
+                        
+                        NavigationLink(destination: Chapter3View()) {
+                            Label ("", systemImage: "arrowshape.right.circle.fill" )
+                        }
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    }
+                }
+                .padding()
             }
-            .padding()
+        
         }
+    
     }
+    
 }
 
 struct ChaptersView1: View {
@@ -127,3 +158,4 @@ struct Chapter2View_Previews: PreviewProvider {
         Chapter2View()
     }
 }
+
